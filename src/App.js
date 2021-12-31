983fc807c706629a408f08e3b1c121a699b73c89
KritzHeng <mr.kriskanin@gmail.com>
2021-12-31 12:07:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+// Base code
+// /* import the ipfs-http-client library */
+// import { create } from 'ipfs-http-client';
+
+// /* Create an instance of the client */
+// const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
+
+// /* upload the file */
+// const added = await client.add(file)
+
+// /* or a string */
+// const added = await client.add('hello world')
+
+
+/* src/App.js */
+import './App.css'
+import { useState } from 'react'
+import { create } from 'ipfs-http-client'
+
+const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function App() {
+  const [fileUrl, updateFileUrl] = useState(``)
+  async function onChange(e) {
+    const file = e.target.files[0]
+    try {
+      const added = await client.add(file)
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`
+      updateFileUrl(url)
+      console.log(url)
+    } catch (error) {
+      console.log('Error uploading file: ', error)
+    }  
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS CREATE</h1>
+      <input
+        type="file"
+        onChange={onChange}
+      />
+      {
+        fileUrl && (
+          <img src={fileUrl} width="600px" />
+        )
+      }
+      <h1>{fileUrl}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
